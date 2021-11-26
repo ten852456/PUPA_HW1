df5bd4885e5004b92c29ea9e5b96ee834bab2f22
Tanawat Palaboon <ten07@hotmail.co.th>
2021-11-26 21:25:56 +0700

    #3 can get from restAPI

--- Front-end/BookstoreWebApp/src/app/bookview/show/show.component.ts
+++ Front-end/BookstoreWebApp/src/app/bookview/show/show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service'
 
 @Component({
   selector: 'app-show',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  Bookviewlist:any=[];
+
+  ModalTitle: string | undefined;
+  ActivateAddEditBook:boolean = false;
+  book:any;
 
   ngOnInit(): void {
+    this.refreshBooklist();
+  }
+
+  addClick(){
+    this.book={
+      BookID:0,
+      Bookname:""
+    }
+    this.ModalTitle="Add Book";
+    this.ActivateAddEditBook=true;
+  }
+  editClick(item: any){
+    this.book=item;
+    this.ModalTitle="Edit Book"
+    this.ActivateAddEditBook=true;
+  }
+
+
+  closeClick(){
+    this.ActivateAddEditBook=false;
+    this.refreshBooklist;
+  }
+
+  refreshBooklist(){
+    this.service.getBooklist().subscribe(data=>{
+      this.Bookviewlist = data;
+    });
   }
 
 }
