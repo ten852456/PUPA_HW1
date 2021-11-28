e5e7160a576cee669a3b052bc1405f8c8a8204ad
Tanawat Palaboon <ten07@hotmail.co.th>
2021-11-28 13:39:47 +0700

    #8 customized model and completed admin page

--- DBbookstore/bookstore/bookdata/models.py
+++ DBbookstore/bookstore/bookdata/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class book(models.Model):
     bookname = models.CharField(max_length=30)
     amount = models.CharField(max_length=10)
-    pic_url = models.CharField(max_length=90)
+    pic_url = models.CharField(max_length=200)
     price = models.CharField(max_length=10)
     def __str__(self):
         return self.bookname
